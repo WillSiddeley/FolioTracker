e5290c4213fd2b7c0b07fa7f8fc57a75502f7804
WillSiddeley <will.iams.sapo@gmail.com>
2022-11-13 11:41:22 -0500

    Initial commit server

---
+++ server/app.js
@@ -0,0 +1,21 @@
+// Imports
+const cors = require("cors");
+const express = require("express");
+const log = require("./services/log");
+
+// Express Settings
+const app = express();
+const port = 3001;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routers
+const quotes = require("./routers/quotes");
+
+// GET Methods
+app.get('/api/v1/quotes/getPrice/:ticker', quotes.getPrice);
+
+// Open Port
+app.listen(port, () => log.logToConsole("App listening on port", port));
