6ec829fb8f5034f3617d47f58343ca5a17dc4777
WillSiddeley <will.iams.sapo@gmail.com>
2022-12-21 16:56:19 -0500

    Updating remainder of file strructure changes

--- src/api/api.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-export default axios.create({
-
-    baseURL: 'http://192.168.2.222:3001'
-    
-});
--- src/constants/WebConstants.js
+++
@@ -1,8 +0,0 @@
-
-
-export default class WebConstants {
-
-    static websiteURL = "http://192.168.2.222:3000";
-    static websiteAPI = "http://192.168.2.222:3001";
-
-}
